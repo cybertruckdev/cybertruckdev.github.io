ae06c06d5cd6bee59726e969c650ecb9552caf92
ChrisHammond <ChrisHammond@users.noreply.github.com>
2023-08-21 03:51:12 +0000

    deploy: f0fa2f3f0f3be871e035e0c3ea4bf9755e7b6e27

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -1,47 +1,47 @@
 
 var documents = [{
     "id": 0,
-    "url": "https://www.Cybertruck.dev/404.html",
+    "url": "https://cybertruck.dev/404.html",
     "title": "404",
     "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "https://www.Cybertruck.dev/about",
+    "url": "https://cybertruck.dev/about",
     "title": "About Cybertruck.dev",
     "body": " Cybertruck. dev is coming soon "
     }, {
     "id": 2,
-    "url": "https://www.Cybertruck.dev/categories",
+    "url": "https://cybertruck.dev/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 3,
-    "url": "https://www.Cybertruck.dev/",
+    "url": "https://cybertruck.dev/",
     "title": "Home",
     "body": "      Featured:                                                                                                                                                                                                             The Cybertruck's Arrival in Missouri - A New Chapter in Automotive Innovation                              :               The Cybertruck’s Arrival in Missouri: A New Chapter in Automotive Innovation:                                                                       20 Aug 2022                                                                                                                                                                                                                                                                                                                    First Post                              :               Welcome to Cybertruck. dev! Stay tuned!:                                                                       20 Aug 2022                                                                                                                      All Stories:                                                                               The Cybertruck's Arrival in Missouri - A New Chapter in Automotive Innovation              :       The Cybertruck’s Arrival in Missouri: A New Chapter in Automotive Innovation:                               20 Aug 2022                                                                                                              First Post              :       Welcome to Cybertruck. dev! Stay tuned!:                               20 Aug 2022                                            "
     }, {
     "id": 4,
-    "url": "https://www.Cybertruck.dev/About",
+    "url": "https://cybertruck.dev/About",
     "title": "",
     "body": ""
     }, {
     "id": 5,
-    "url": "https://www.Cybertruck.dev/redirects.json",
+    "url": "https://cybertruck.dev/redirects.json",
     "title": "",
-    "body": "{“/About”:”https://www. cybertruck. dev/about”} "
+    "body": "{“/About”:”https://cybertruck. dev/about”} "
     }, {
     "id": 6,
-    "url": "https://www.Cybertruck.dev/robots.txt",
+    "url": "https://cybertruck.dev/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 7,
-    "url": "https://www.Cybertruck.dev/cybertruck-missouri",
+    "url": "https://cybertruck.dev/cybertruck-missouri",
     "title": "The Cybertruck's Arrival in Missouri - A New Chapter in Automotive Innovation",
     "body": "2022/08/20 - The Cybertruck’s Arrival in Missouri: A New Chapter in Automotive InnovationMissouri, long known for its contributions to the automotive industry, is soon to become one of the latest states to welcome Tesla’s futuristic electric vehicle, the Cybertruck. Set to disrupt the trucking market with its radical design and cutting-edge technology, the Cybertruck is poised to make a significant impact in the Show-Me State. Tesla’s Cybertruck: A Quick Overview: The Cybertruck, announced by Tesla in 2019, is a fully electric pickup truck that challenges the conventional automotive design. With its angular, stainless steel exterior and innovative electric drivetrain, the Cybertruck has captured the imagination of auto enthusiasts and tech-savvy consumers alike. Featuring a 100 cubic feet of exterior, lockable storage, the Cybertruck aims to provide utility and performance without compromising on environmental sustainability. With different models offering a range of up to 500 miles on a single charge and the ability to tow over 14,000 pounds, it is seen as a serious competitor to traditional fossil fuel-powered trucks. Missouri’s Automotive History: Missouri has a rich history in the automotive industry. Kansas City and St. Louis have been home to several major car manufacturing plants, contributing significantly to the economy and technological development in the area. The arrival of the Cybertruck is seen as a continuation of this tradition, marking a new era of innovation and progress in automotive manufacturing and electric vehicle adoption. The Demand for Electric Vehicles in Missouri: In recent years, Missouri has seen a growing interest in electric vehicles (EVs). The state government has been proactive in supporting EV adoption through incentives, charging infrastructure, and collaboration with industry leaders. With the Cybertruck’s arrival, there is anticipation for a surge in consumer interest in electric trucks, particularly among those involved in agriculture, construction, and other industries that rely heavily on pickup trucks. The Cybertruck’s blend of power and efficiency is expected to appeal to a wide range of consumers across the state. Economic Impact: The introduction of the Cybertruck to Missouri may also have significant economic implications. Beyond the consumer market, the Cybertruck may find applications in various commercial sectors, potentially generating new business opportunities and jobs. Local dealerships and service centers may also see growth, as they adapt to the increasing demand for electric vehicles and the specific needs of Cybertruck owners. Collaboration between Tesla and local educational institutions for training and research could foster innovation and skill development in the region. Challenges and Concerns: While excitement surrounds the Cybertruck’s arrival, there are challenges and concerns to address. The unique design of the Cybertruck may require adaptations in service infrastructure. The continued expansion of charging stations will be crucial to support new Cybertruck owners. Moreover, the state’s readiness to embrace this new technology in terms of regulations, safety standards, and public perception may shape the Cybertruck’s success in Missouri. Conclusion: The Cybertruck’s arrival in Missouri is more than a new vehicle entering the market; it’s a symbol of the ongoing shift toward sustainable transportation and a new chapter in the state’s proud automotive history. The success of the Cybertruck in Missouri will depend on various factors, including consumer acceptance, infrastructure development, and collaboration between Tesla and local stakeholders. However, the excitement and anticipation surrounding this futuristic vehicle suggest that the Show-Me State is ready to embrace the Cybertruck as a part of its automotive future. Picture Source "
     }, {
     "id": 8,
-    "url": "https://www.Cybertruck.dev/first-post",
+    "url": "https://cybertruck.dev/first-post",
     "title": "First Post",
     "body": "2022/08/20 - Welcome to Cybertruck. dev! Stay tuned! (Photo courtesy of Ed Runnion) "
     }];
